7067dc5911a43dc99ec4ac3eb60e793d12807b91
Maksym Zholtikov <maksym.zholtikov@gmail.com>
2014-11-02 23:34:50 +0200

    First pages commit

--- app/scripts/services/selectitem.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('testApp').filter('selectItem', ['$sce', function ($sce) {
-  return function (input, search) {
-    if (typeof input === 'function') return '';
-    return $sce.trustAsHtml(input);
-  };
-}]);
